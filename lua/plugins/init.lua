e2f0bdd344f14d3a6fcccbe91f868be4ed08c7a3
jeffyongtaotang <jeff.tong@konghq.com>
2025-02-19 17:19:06 -0700

    refactor(dap-ui) static console to right

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -56,7 +56,7 @@ return {
           vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
           vim.keymap.set("n", "o", api.node.open.edit, opts("Open"))
           vim.keymap.set("n", "<CR>", api.node.open.edit, opts("Open"))
-          vim.keymap.set("n", "v", api.node.open.vertical, opts("Open: Verical Split"))
+          vim.keymap.set("n", "v", api.node.open.vertical, opts("Open: Vertical Split"))
           vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close Directory"))
           vim.keymap.set("n", "C", api.tree.change_root_to_node, opts("CD"))
         end,
@@ -168,13 +168,7 @@ return {
           "<leader>du",
           function()
             local dap_ui = require("dapui")
-            dap_ui.float_element("console", {
-              title = "Console",
-              position = "center",
-              width = 80,
-              height = 100,
-              enter = true,
-            })
+            dap_ui.toggle({ layout = 1, reset = true })
           end,
           desc = "Dap Console",
         },
@@ -259,13 +253,8 @@ return {
       local dap = require("dap")
       local dapui = require("dapui")
       local open_console = function()
-        dapui.float_element("console", {
-          title = "Console",
-          width = 80,
-          height = 100,
-          enter = true,
-          position = "center",
-        })
+        dapui.close({ layout = 1 })
+        dapui.toggle({ layout = 1, reset = true })
       end
 
       dapui.setup(opts)
