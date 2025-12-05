a825ac3348f0da37c90787b0dd7b97b4f1be1c06
jeffyongtaotang <jeff.tong@konghq.com>
2025-12-05 13:38:34 -0700

    don't update tree-sitter

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,6 +17,7 @@ require("lazy").setup({
       import = "lazyvim.plugins",
       opts = { colorscheme = "nightfly" },
     },
+    { "nvim-treesitter/nvim-treesitter", lazy = false, branch = "master", build = ":TSUpdate" },
     { import = "lazyvim.plugins.extras.editor.mini-files" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "plugins" },
